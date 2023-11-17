8835941498cb1ccf90d9154066b43ce5e18d4feb
Loafaroo <53982793+Loafaroo@users.noreply.github.com>
2023-11-16 19:21:03 -0600

    main selector icon with fill

--- updateColorList.js
+++ updateColorList.js
@@ -1,14 +1,26 @@
 function updateColorList(selectedColor) {
 	const listItems = document.querySelectorAll('#colorList li');
 	const selectedRgbColor = hexToRgb(selectedColor);
+
+	
+	
 	
 	listItems.forEach(item => {
-		if (item.querySelector('.color-box').style.backgroundColor === selectedRgbColor) {
-		item.style.backgroundColor = 'white';
-		item.style.color = 'black';
+		const colorBox = item.querySelector('.color-box');
+		if (colorBox.style.backgroundColor === selectedRgbColor) {
+			//highlight the selected color
+			item.style.backgroundColor = 'white';
+			item.style.color = 'black';
+			// and shade the border of the color box to contrast
+			colorBox.style.border = '1px solid black';
+		
 		} else {
-			item.style.backgroundColor = '';
-			item.style.color = '#fff';
+			item.style.backgroundColor = 'rgba(119,130,145,0.0)';
+			item.style.color = 'white';
+			
+			colorBox.style.border = '1px solid white';
+			// console.log(item.querySelector('.color-box'));
+			
 		}
 	});
 }
